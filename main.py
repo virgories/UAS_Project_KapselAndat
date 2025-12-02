dbae9a8c3c3475c8b41f489323b45000bb0d2f4d
virgories <yangyangagatha@gmail.com>
2025-12-02 11:43:19 +0700

    edit main

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-# main.py
 from fastapi import FastAPI
-# Cukup import init_db untuk membuat tabel
 from database import init_db, SessionLocal, seed_categories
-# Import router Anda
-from routers import categories, data_uas_router
-# Import router analytics
-from routers.analytics import router as analytics_router
 import models
 
-# 1. Panggil fungsi inisialisasi database (membuat tabel)
-# Ini harus dipanggil sekali sebelum aplikasi berjalan
-init_db() 
+from routers import categories, data_uas_router, transaction, analytics
 
-app = FastAPI()
+# bikin tabel kalau belum ada
+init_db()
+
+app = FastAPI(
+    title="Warehouse Management API",
+    version="1.0.0"
+)
+
+# seed kategori (kalau memang butuh)
 db_session = SessionLocal()
 seed_categories(db_session, models)
 
-# 2. Sertakan semua router
+# include SEMUA router
 app.include_router(categories.router)
 app.include_router(data_uas_router.router)
-app.include_router(analytics_router)
+app.include_router(transaction.router)   # CRUD transaksi
+app.include_router(analytics.router)    # Analytics
